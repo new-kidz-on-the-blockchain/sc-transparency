09e7287741476aadb00eaad7e86e7a2c349ba9d5
psei3112 <philipp.seitz@kaufland.com>
2017-09-29 09:59:18 +0200

    input fix

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RouterModule, Routes, Router } from '@angular/router';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {RouterModule, Routes, Router} from '@angular/router';
 import {MdSnackBar} from '@angular/material';
 
 @Component({
@@ -11,46 +11,31 @@ import {MdSnackBar} from '@angular/material';
 export class InputComponent implements OnInit {
 
 
-   rForm: FormGroup;
-   post:any;                     // A property for our submitted form
-   description:string = '';
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  description = '';
 
 
-   constructor(private fb: FormBuilder, private router: Router, public snackBar: MdSnackBar) {
-     this.rForm = fb.group({
-       'productCode' : [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(50)])]
-     });
-   }
+  constructor(private fb: FormBuilder, private router: Router, public snackBar: MdSnackBar) {
+    this.rForm = fb.group({
+      'productCode': [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(50)])]
+    });
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    onSubmitClick(post) {
-      var enteredProductCode = this.rForm.getRawValue().productCode;
-      alert(enteredProductCode);
-      this.router.navigate(['view', {productid:enteredProductCode}]);
+  onSubmitClick(post) {
+    const enteredProductCode = this.rForm.getRawValue().productCode;
+    this.router.navigate(['view'], {queryParams: {productid: enteredProductCode}});
     //  this.router.navigate(['view']);
     this.description = post.description;
   }
 
 
   decodedOutput(scannedResult) {
-    //alert(scannedResult);
+    // alert(scannedResult);
     this.snackBar.open(scannedResult);
-    this.router.navigate(['view', {productid:scannedResult}]);
+    this.router.navigate(['view'], {queryParams: {productid: scannedResult}});
   }
-
-  onScanClick() {
-    // let video = this.hardwareVideo.nativeElement;
-    //
-    //    var n = <any>navigator;
-    //
-    //    n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
-    //
-    //    n.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    //        video.src = window.URL.createObjectURL(stream);
-    //        video.play();
-    //    });
-}
-
 }
