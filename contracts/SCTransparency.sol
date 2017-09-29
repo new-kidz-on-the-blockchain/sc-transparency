7f3e6fa84c2e84fcf4a29e2e572c8fb75506f38f
Felix Straub <straub.felix@yahoo.de>
2017-09-29 11:42:08 +0200

    Added owner, supplier, productSupplier Modifier. Added Modifier to functions. Just owner can add suppliers. Just suppliers can add lifecyclePoint. Just productSupplier can throw events for lifecyclePoint. Tests went well!!

--- contracts/SCTransparency.sol
+++ contracts/SCTransparency.sol
@@ -6,6 +6,7 @@
       string productName;
       uint[] origin;
       string location;
+      address supplier;
     }
 
     address private owner;
@@ -15,12 +16,27 @@
     }
 
     mapping (uint => uint) public qrCodeToTx;
-    mapping (address => string) public supplier;
+    mapping (address => string) public suppliers;
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+    }
+
+    modifier isSupplier{
+      require(bytes(suppliers[msg.sender]).length != 0);
+      _;
+    }
+
+    modifier onlyProductSupplier(uint _txid){
+      require(lifeCyclePoints[_txid].supplier == msg.sender);
+      _;
+    }
 
     LifeCyclePoint[] lifeCyclePoints;
 
-    function addLifecyclePoint(string _productName, uint[] _origin, string _location) {
-      LifeCyclePoint memory p = LifeCyclePoint(_productName, _origin, _location);
+    function addLifecyclePoint(string _productName, uint[] _origin, string _location) isSupplier{
+      LifeCyclePoint memory p = LifeCyclePoint(_productName, _origin, _location, msg.sender);
       lifeCyclePoints.push(p);
       Id(tx.origin, lifeCyclePoints.length-1);
     }
@@ -33,8 +49,8 @@
       return qrCodeToTx[_qrcode];
     }
 
-    function addSupplier(address _sup, string _name){
-      supplier[_sup] = _name;
+    function addSupplier(address _sup, string _name) onlyOwner{
+      suppliers[_sup] = _name;
     }
 
     event Id(address _origin, uint id);
@@ -42,14 +58,14 @@
     event Change(uint _txid,string _attribute, string _value);
     event Recall(uint _txid, string _reason);
 
-    function addInfo(uint _txid, string _info){
+    function addInfo(uint _txid, string _info) onlyProductSupplier(_txid){
       Info(_txid, _info);
     }
 
-    function addRecall(uint _txid, string _reason){
+    function addRecall(uint _txid, string _reason) onlyProductSupplier(_txid){
       Recall(_txid, _reason);
     }
-    function addChange(uint _txid, string _attribute, string _value){
+    function addChange(uint _txid, string _attribute, string _value) onlyProductSupplier(_txid){
       Change(_txid, _attribute, _value);
     }
 
--- test/SCTransparencyTest.js
+++ test/SCTransparencyTest.js
@@ -2,23 +2,27 @@ let Kontract = artifacts.require('./SCTransparency.sol');
 const InputDataDecoder = require('ethereum-input-data-decoder');
 let Web3 = require('web3');
 let web3 = new Web3('http://localhost:8545/');
+let kontract;
 
 contract('Kontract', function(accounts) {
 
 
   it('should get transaction data', async function() {
-      let kontract = await Kontract.deployed();
-      const decoder = new InputDataDecoder(kontract.abi);
-      let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[0]});
+      kontract = await Kontract.deployed();
+      await kontract.addSupplier(accounts[1], 'Coca Cola');
+      sName = await kontract.suppliers.call(accounts[1]);
+      assert.equal(sName, 'Coca Cola', 'No supplier added');
 
+      let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[1]});
       let id = extractId(transactionAddr);
-      
-      let origin = await kontract.getOrigin.call(id); 
-      console.log(origin);
+
+      let origin = await kontract.getOrigin.call(id);
       assert.equal(await kontract.getProductName.call(id),'Milch',"No milk in the Blockchain!");
-      assert.equal(await kontract.getLocation.call(id),   'WSB',"Milk ist not from WSB!");
-      assert.deepEqual(origin,[],"origin is fishy");
+    assert.deepEqual(origin,[],"origin is fishy");
+    assert.equal(await kontract.getLocation.call(id),   'WSB',"Milk ist not from WSB!");
   });
+
+
 
   it('should get TxID from QR Code', async function () {
     let kontract = await Kontract.deployed();
@@ -28,9 +32,14 @@ contract('Kontract', function(accounts) {
     assert.equal(txHash,0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd,"Wrong TxID");
   });
 
+
+
   it('should get TxID from QR Code where TxID should be from Product', async function () {
     let kontract = await Kontract.deployed();
-    let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB');
+    await kontract.addSupplier(accounts[1], 'Coca Cola');
+    sName = await kontract.suppliers.call(accounts[1]);
+    assert.equal(sName, 'Coca Cola', 'No supplier added');
+    let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[1]});
     let id = extractId(transactionAddr);
     kontract.mapQrCodeToTxid(1234567891,id);
     let txid = await kontract.getTxidFromQrCode.call(1234567891);
@@ -42,6 +51,85 @@ contract('Kontract', function(accounts) {
 
 
 
+  it('should check supplier added', async function () {
+    let kontract = await Kontract.deployed();
+    await kontract.addSupplier(0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd, 'Nestle');
+    let sName = await kontract.suppliers.call(0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd);
+    assert.equal(sName, 'Nestle', 'No supplier added');
+  })
+
+
+
+  it('should throw error for not owner', async function () {
+    let kontract = await Kontract.deployed();
+    await kontract.addSupplier(0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd, 'Nestle');
+    let sName = await kontract.suppliers.call(0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd);
+    assert.equal(sName, 'Nestle', 'No supplier added');
+    await kontract.addSupplier(accounts[1], 'Coca Cola');
+    sName = await kontract.suppliers.call(accounts[1]);
+    assert.equal(sName, 'Coca Cola', 'No supplier added');
+
+    try{
+      await kontract.addSupplier(0x7d07c0a053eb788e4bee9b61cc207ce2c1b85d879f999454dc9cba0ae6c2cfbd, 'Nestle',{from:accounts[1]});
+    }catch (error){
+      assert.equal(error.message, 'VM Exception while processing transaction: invalid opcode', 'Did not throw due to not owner');
+    }
+  })
+
+
+
+  it('should throw error for not supplier', async function () {
+    let kontract = await Kontract.deployed();
+    await kontract.addSupplier(accounts[1], 'Coca Cola');
+
+    try{
+      await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[2]});
+    }catch (error){
+      assert.equal(error.message, 'VM Exception while processing transaction: invalid opcode', 'Did not throw due to not a supplier');
+    }
+
+    let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[1]});
+    let id = extractId(transactionAddr);
+
+    let origin = await kontract.getOrigin.call(id);
+    assert.equal(await kontract.getProductName.call(id),'Milch',"No milk in the Blockchain!");
+    assert.deepEqual(origin,[],"origin is fishy");
+
+  })
+
+  it('should throw error for not supplier of product', async function () {
+    let kontract = await Kontract.deployed();
+    await kontract.addSupplier(accounts[1], 'Coca Cola');
+
+    let transactionAddr = await kontract.addLifecyclePoint('Milch', [], 'WSB', {from: accounts[1]});
+    let id = extractId(transactionAddr);
+
+    let origin = await kontract.getOrigin.call(id);
+    assert.equal(await kontract.getProductName.call(id),'Milch',"No milk in the Blockchain!");
+    assert.deepEqual(origin,[],"origin is fishy");
+
+    try{
+      await kontract.addInfo(id,'Sonderangebot ööö');
+      await kontract.addChange(id,'Location', 'NSU');
+      await kontract.addRecall(id,'Salmonellen pfui');
+    }catch (error){
+      assert.equal(error.message, 'VM Exception while processing transaction: invalid opcode', 'Did not throw due to not a supplier');
+    }
+
+    try{
+      await kontract.addChange(id,'Location', 'NSU');
+    }catch (error){
+      assert.equal(error.message, 'VM Exception while processing transaction: invalid opcode', 'Did not throw due to not a supplier');
+    }
+
+    try{
+      await kontract.addRecall(id,'Salmonellen pfui');
+    }catch (error){
+      assert.equal(error.message, 'VM Exception while processing transaction: invalid opcode', 'Did not throw due to not a supplier');
+    }
+
+  })
+
 });
 
 function extractId(transactionAddr){
@@ -50,6 +138,6 @@ function extractId(transactionAddr){
 	  if (log.event == "Id"){
 	      return log.args.id;
 	  }
-	  
       }
 }
+
